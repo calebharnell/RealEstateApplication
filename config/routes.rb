12df499c298fedc6df8c122bf834ea56adf151bb
calebharnell <calebharnell@hotmail.com>
2017-10-19 14:33:58 +1000

    Update devise params

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'houses#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :houses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
